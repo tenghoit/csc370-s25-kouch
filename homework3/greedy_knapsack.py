8e77cbe6700f74a727ce28470c53b16e1a8b337f
Tenghoit Kouch <tenghoitkouch+github@gmail.com>
2025-03-01 22:55:06 -0500

    test args

--- homework3/greedy_knapsack.py
+++ homework3/greedy_knapsack.py
@@ -3,32 +3,31 @@ import sys
 
 class Item:
     def __init__(self, item_name: str, item_weight: int, item_value: int) -> None:
-        self.item_name = item_name
+        self.item_name: str = item_name
         self.item_weight: int = item_weight
         self.item_value: int = item_value
 
-    def print(self):
+    def print(self) -> None:
         print(f'Item Name: {self.item_name} | Item Weight: {self.item_weight} | Item Value: {self.item_value}')
 
 
-def print_items(items: list[Item]) -> None:
+def print_items(items: list[Item], result: list[Item]) -> None:
 
     for item in items:
-        print(f'{item.item_name.rstrip()}\n{item.item_weight}\n{item.item_value}')
+        if item in result:
+            print(f'{item.item_name}\n{item.item_weight}\n{item.item_value}')
 
 
 def greedy_value(items: list[Item], weight_limit: int) -> None:
 
-    items_sorted = sorted(items, key=lambda p: p.item_value, reverse=True)
-
-    # print_items(items_sorted)
+    items_sorted = sorted(items, key=lambda p: p.item_value)
 
     result = []
     current_weight = 0
 
     while len(items_sorted) > 0:
 
-        item = items_sorted.pop(0)
+        item = items_sorted.pop()
 
         # print_items(items_sorted)
         # print()
@@ -39,20 +38,22 @@ def greedy_value(items: list[Item], weight_limit: int) -> None:
             result.append(item)
             current_weight += item.item_weight
 
+            # print(f'Current weight: {current_weight}')
+
     
-    print_items(result)
+    print_items(items, result)
 
 
 def greedy_value_to_weight(items: list[Item], weight_limit: int) -> None:
 
-    items_sorted = sorted(items, key=lambda p: p.item_value / p.item_weight, reverse=True)
+    items_sorted = sorted(items, key=lambda p: (p.item_value / p.item_weight) )
 
     result = []
     current_weight = 0
 
     while len(items_sorted) > 0:
 
-        item = items_sorted.pop(0)
+        item = items_sorted.pop()
 
         if(current_weight + item.item_weight > weight_limit):
             continue
@@ -61,10 +62,13 @@ def greedy_value_to_weight(items: list[Item], weight_limit: int) -> None:
             current_weight += item.item_weight
 
     
-    print_items(result)
+    print_items(items, result)
 
 
 def main():
+
+    print(f'Len of args: {len(sys.argv)}')
+    print(sys.argv)
     
     if len(sys.argv) != 5:
         print('Invalid Args Count')
@@ -93,10 +97,11 @@ def main():
             item_weight = int(file.readline().strip())
             item_value = int(file.readline().strip())
 
-            items.append(Item(item_name=item_name, item_weight=item_weight, item_value=item_value))
+            items.append(Item(item_name, item_weight, item_value))
 
-
-    # print_items(items)
+    # print(f'Len of Items: {len(items)}')
+    # print_items(items, items)
+    # print()
 
     if heuristic_name == 'by_value':
         greedy_value(items, weight_limit)
@@ -105,5 +110,4 @@ def main():
 
 
 
-if __name__ == '__main__':
-    main()
+main()
